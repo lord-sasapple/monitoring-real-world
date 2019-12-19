931c5a24f84044f558c16f5cf0cc29f73726b7c1
Steve-Sasaki <wmaaaa.sa@gmail.com>
2019-12-20 04:31:06 +0900

    PORT を herokuでも動くように変更

--- io/index.js
+++ io/index.js
@@ -12,7 +12,10 @@ export default function () {
         const io = socketIO(server)
 
         // overwrite nuxt.server.listen()
-        this.nuxt.server.listen = (port, host) => new Promise(resolve => server.listen(port || 3000, host || 'localhost', resolve))
+        const PORT = process.env.PORT || 3000;
+        this.nuxt.server.listen = (port, host) => new Promise(resolve => {
+            server.listen(PORT, host || 'localhost', resolve)
+        })
         // close this server on 'close' event
         this.nuxt.hook('close', () => new Promise(server.close))
 
