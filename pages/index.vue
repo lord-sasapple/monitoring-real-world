950055c1fd004a1d51cce579ffd6387630d5689b
Steve-Sasaki <wmaaaa.sa@gmail.com>
2019-12-19 21:47:16 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import { Person } from "~/types";
+
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
